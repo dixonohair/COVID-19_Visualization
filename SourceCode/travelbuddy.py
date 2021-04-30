05ddf9a465ceea20b1f43c137876f8b2eab71720
egomez4 <egomez4@uncc.edu>
2021-04-30 19:23:29 -0400

    couldnt get the nav stuff to work :(

--- SourceCode/travelbuddy.py
+++ SourceCode/travelbuddy.py
@@ -1,15 +1,16 @@
-import json
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 
 # load datasets
 df = pd.read_csv('WHOcovidglobal.csv')
 cases_deaths_df = pd.read_csv('covidcases.csv')
+vaccinations_df = pd.read_csv('vaccinations.csv')
+travel_advisories_df = pd.read_csv('covid19TravelRestrictionsCountry.csv')
 
 # Initialise the app
 app = dash.Dash(__name__)
@@ -23,8 +24,8 @@ fig = go.Figure(data=go.Choropleth(
     marker_line_width=1,
     colorscale='tealgrn',
     colorbar=dict(title='Cumulative COVID-19 Cases'),
-    zmin=0,
-    zmax=25000000
+    zmin=100000,
+    zmax=20000000
 ))
 
 # the base map
@@ -54,10 +55,19 @@ app.layout = html.Div(children=[
     html.H1('Travel Buddy', style={
         'color': 'white',
         'font-size': '48',
-        'font': 'Arial, sans-serif'
+        'padding': 10,
+        'width': 'auto',
+        'background-color': '#303030',
+        'box-shadow': '2px 2px 5px #191818'
     }),
 
-    # div containing the graph
+    html.H1('Global Covid Data: At a Glance', style={
+        'color': 'white',
+        'font-size': 50,
+        'margin-left': '50px'
+    }),
+
+    # div containing the world map
     html.Div(children=[
 
         # the graph
@@ -65,8 +75,8 @@ app.layout = html.Div(children=[
             'display': 'inline-block',
             'margin': 'auto',
             "overflow": "hidden",
-            'border-radius': 20,
-            'box-shadow': '2px 2px 10px  #191818'
+            'box-shadow': '2px 2px 10px  #191818',
+            'border-radius': '5px'
         }),
 
     ], style={
@@ -77,21 +87,46 @@ app.layout = html.Div(children=[
 
     html.Br(),
 
+    html.H1('Covid Data by Country', style={
+        'color': 'white',
+        'margin-left': '50px',
+        'font-size': 48
+    }),
+    html.Br(),
+
     # text input
-    dcc.Input(id='country-search', type='text', value='Afghanistan', placeholder='Where would you like to go?', style={
+    dcc.Input(id='country-search', type='text', value='Japan', placeholder='Enter a country...', style={
         'display': 'block',
         'margin': 'auto',
         'width': '50%',
         'height': 30,
-        'border-radius': 20,
         'border-style': 'none',
-        'box-shadow': '2px 2px 10px  #191818'
+        'box-shadow': '2px 2px 10px  #191818',
+        'border-radius': 20
     }),
     html.Br(),
-
+    html.H1('Travel Advisory', style={
+        'color': 'white',
+        'margin-left': '90px',
+        'font-size': '36px'
+    }),
     # div containing information that was searched for
+    html.Div(id='travel advisories'),
+    html.Br(),
+
+    html.H1('Covid Data', style={
+        'color': 'white',
+        'margin-left': '90px',
+        'font-size': '36px'
+    }),
+
+    # div for new cases and deaths chart
     html.Div(dcc.Graph(id='new cases and deaths')),
-    html.Div(dcc.Graph(id='cumulative cases and deaths'))
+    html.Br(),
+
+    # div for cumulative cases and deaths chart
+    html.Div(dcc.Graph(id='cumulative cases and deaths')),
+    html.Br()
 ]
 
     # style for main div containing all other components (the background)
@@ -99,24 +134,24 @@ app.layout = html.Div(children=[
         'background-color': '#404040',
         'height': 'auto',
         'width': 'auto',
-        'padding-bottom': 10
+        'padding-bottom': 10,
+        'font-family': 'Segoe UI, sans-serif'
     })
 
 
 # returns linechart for covid deaths and cases of a country
 # output is returned to graph with id of 'cases and deaths'
 # input taken from search bar
-@app.callback(
-    Output('new cases and deaths', 'figure'),
-    Input('country-search', 'value')
-)
+@app.callback(Output('new cases and deaths', 'figure'), Input('country-search', 'value'))
 def new_cases_and_deaths_linechart(country_search):
     # filter data for selected country
     filtered_df = cases_deaths_df[cases_deaths_df['Country'] == country_search]
 
     # create line chart
-    trace1 = go.Scatter(x=filtered_df['Date_reported'], y=filtered_df['New_cases'], mode='lines', name='New Cases')
-    trace2 = go.Scatter(x=filtered_df['Date_reported'], y=filtered_df['New_deaths'], mode='lines', name='New Deaths')
+    trace1 = go.Scatter(x=filtered_df['Date_reported'], y=filtered_df['New_cases'], mode='lines', name='New Cases',
+                        line={'color': 'salmon', 'width': 2})
+    trace2 = go.Scatter(x=filtered_df['Date_reported'], y=filtered_df['New_deaths'], mode='lines', name='New Deaths',
+                        line={'color': '#ccccff', 'width': 2})
     linedata = [trace1, trace2]
 
     return {'data': linedata, 'layout': go.Layout(
@@ -128,19 +163,17 @@ def new_cases_and_deaths_linechart(country_search):
         legend={'font_color': 'white'}
     )}
 
-@app.callback(
-    Output('cumulative cases and deaths', 'figure'),
-    Input('country-search', 'value')
-)
+
+@app.callback(Output('cumulative cases and deaths', 'figure'), Input('country-search', 'value'))
 def cumulative_cases_and_deaths_linechart(country_search):
     # filter data for selected country
     filtered_df = cases_deaths_df[cases_deaths_df['Country'] == country_search]
 
     # create line chart
     trace1 = go.Scatter(x=filtered_df['Date_reported'], y=filtered_df['Cumulative_cases'], mode='lines',
-                        name='Cumulative Cases')
+                        name='Cumulative Cases', line={'color': '#8cf5b4', 'width': 2})
     trace2 = go.Scatter(x=filtered_df['Date_reported'], y=filtered_df['Cumulative_deaths'], mode='lines',
-                        name='Cumulative Deaths')
+                        name='Cumulative Deaths', line={'color': '#e2ff68', 'width': 2})
     linedata = [trace1, trace2]
 
     return {'data': linedata, 'layout': go.Layout(
@@ -149,8 +182,23 @@ def cumulative_cases_and_deaths_linechart(country_search):
         plot_bgcolor='#404040',
         yaxis={'color': 'white'},
         xaxis={'color': 'white'},
-        legend={'font_color': 'white'}
+        legend={'font_color': 'white'},
     )}
+
+
+# callback to get travel advisory for country
+@app.callback(Output('travel advisories', 'children'), Input('country-search', 'value'))
+def travel_advisories(country_search):
+    # filter data for selected country
+    filtered_df = travel_advisories_df[travel_advisories_df['Country'] == country_search]
+
+    # return paragraph containing latest news
+    return html.P(filtered_df['info'], style={
+        'color': 'white',
+        'width': '80%',
+        'margin': 'auto',
+        'display': 'block'
+    })
 
 
 # Run the app
